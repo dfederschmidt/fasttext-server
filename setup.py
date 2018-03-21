ee76245e16d1ae4123fac4c348203fc8d649a688
Daniel Federschmidt <daniel@federschmidt.xyz>
2018-03-21 11:47:35 +0100

    test version bump

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
-from pipenv.project import Project
-from pipenv.utils import convert_deps_to_pip
 
 setup(name='fasttext-server',
-      version='0.1',
+      version='0.1.1',
       description='Deploy fasttext models',
       author='Daniel Federschmidt',
       author_email='daniel@federschmidt.xyz',
       url='https://federschmidt.xyz',
-      packages=['ft_server', 'test']
+      packages=['ft_server', 'test'],
+      install_requires=["click", "flask"],
+      dependency_links=["https://github.com/facebookresearch/fastText#egg=fasttext"]
       )
