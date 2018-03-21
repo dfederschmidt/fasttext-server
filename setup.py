ba625bff0f9376122a88354b52e80e8da2a531d7
Daniel Federschmidt <daniel@federschmidt.xyz>
2018-03-21 12:24:19 +0100

    uppercase

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='fasttext-server',
-      version='0.1.2',
+      version='0.1.7',
       description='Deploy fasttext models',
       author='Daniel Federschmidt',
       author_email='daniel@federschmidt.xyz',
       url='https://federschmidt.xyz',
       packages=['ft_server', 'test'],
-      install_requires=["click", "flask"],
-      dependency_links=["git+https://github.com/facebookresearch/fastText#egg=fasttext"]
+      install_requires=["click", "flask", "cython", "fastText"],
+      dependency_links=["git+https://github.com/facebookresearch/fastText.git@master#egg=fastText-9"]
       )
