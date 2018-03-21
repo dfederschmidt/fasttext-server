62886b66a2c7521e4d58e1ca6429fc25c4e346b9
Daniel Federschmidt <daniel@federschmidt.xyz>
2018-03-21 11:26:37 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
 
 setup(name='fasttext-server',
       version='0.1',
@@ -6,5 +8,5 @@ setup(name='fasttext-server',
       author='Daniel Federschmidt',
       author_email='daniel@federschmidt.xyz',
       url='https://federschmidt.xyz',
-      packages=['ft_server', 'test'],
+      packages=['ft_server', 'test']
       )
