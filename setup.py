d2ce8e17c80b462817362c732a105a93dbc12194
Daniel Federschmidt <daniel@federschmidt.xyz>
2018-03-21 12:14:18 +0100

    try git+https

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='fasttext-server',
-      version='0.1.1',
+      version='0.1.2',
       description='Deploy fasttext models',
       author='Daniel Federschmidt',
       author_email='daniel@federschmidt.xyz',
       url='https://federschmidt.xyz',
       packages=['ft_server', 'test'],
       install_requires=["click", "flask"],
-      dependency_links=["https://github.com/facebookresearch/fastText#egg=fasttext"]
+      dependency_links=["git+https://github.com/facebookresearch/fastText#egg=fasttext"]
       )
