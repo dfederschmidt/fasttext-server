72fddc915db9370a46710aeac55e00407012a3d5
Daniel Federschmidt <daniel@federschmidt.xyz>
2018-03-26 17:37:10 +0200

    fix datafile syntax

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='fasttext-server',
-      version='0.1.9',
+      version='0.1.10',
       description='Deploy fasttext models',
       author='Daniel Federschmidt',
       author_email='daniel@federschmidt.xyz',
       url='https://federschmidt.xyz',
       packages=['ft_server', 'test'],
       install_requires=["click", "flask"],
-      data_files=[("config.py", "config.py")]
+      data_files=[("config.py", ["config.py"])]
       )
